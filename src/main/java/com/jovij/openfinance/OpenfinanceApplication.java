d40e74909b157b2ba37de2c1af8ae3b7b0b13f04
jovi-j <nean@protonmail.com>
2023-02-02 01:16:35 -0300

    initial commit

---
+++ src/main/java/com/jovij/openfinance/OpenfinanceApplication.java
@@ -0,0 +1,13 @@
+package com.jovij.openfinance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenfinanceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpenfinanceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jovij/openfinance/OpenfinanceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jovij.openfinance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenfinanceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

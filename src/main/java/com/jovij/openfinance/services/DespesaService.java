420ebd9a4fefb01cab6251e81d42a485feb3ce24
jovi-j <nean@protonmail.com>
2023-02-07 16:35:18 -0300

    feat: paginação no findAll

--- src/main/java/com/jovij/openfinance/services/DespesaService.java
+++ src/main/java/com/jovij/openfinance/services/DespesaService.java
@@ -3,6 +3,10 @@ package com.jovij.openfinance.services;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
@@ -22,8 +26,15 @@ public class DespesaService {
     }
 
     @Transactional(readOnly = true)
-    public ResponseEntity<List<Despesa>> findAll() {
-        return new ResponseEntity<List<Despesa>>(repo.findAll(), HttpStatus.OK);
+    public ResponseEntity<List<Despesa>> findAll(Integer pageNo, Integer pageSize, String sortBy) {
+        Pageable paging = PageRequest.of(pageNo, pageSize, Sort.by(sortBy));
+        Page<Despesa> pagedResult = repo.findAll(paging);
+        
+        if(pagedResult.hasContent()){
+            return new ResponseEntity<List<Despesa>>(pagedResult.getContent(), HttpStatus.OK);
+        }else{
+            return new ResponseEntity<List<Despesa>>(HttpStatus.OK);
+        }
     }
 
 

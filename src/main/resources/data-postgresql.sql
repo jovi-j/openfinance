b22a55d48307b68c02bff7adff39830cf90127b4
jovi-j <nean@protonmail.com>
2023-02-08 20:28:44 -0300

    fix:alterações da parte de inicialização de banco de dados

---
+++ src/main/resources/data-postgresql.sql
@@ -0,0 +1,17 @@
+INSERT INTO despesa (id, data_da_despesa, titulo, valor) VALUES (1, now(), 'teste',800.0);
+INSERT INTO despesa (id, data_da_despesa, titulo, valor) VALUES (2, now(), 'teste',800.0);
+INSERT INTO despesa (id, data_da_despesa, titulo, valor) VALUES (3, now(), 'teste',800.0);
+INSERT INTO despesa (id, data_da_despesa, titulo, valor) VALUES (4, now(), 'teste',800.0);
+INSERT INTO despesa (id, data_da_despesa, titulo, valor) VALUES (5, now(), 'teste',800.0);
+INSERT INTO despesa (id, data_da_despesa, titulo, valor) VALUES (6, now(), 'teste',800.0);
+INSERT INTO despesa (id, data_da_despesa, titulo, valor) VALUES (7, now(), 'teste',800.0);
+INSERT INTO despesa (id, data_da_despesa, titulo, valor) VALUES (8, now(), 'teste',800.0);
+INSERT INTO despesa (id, data_da_despesa, titulo, valor) VALUES (9, now(), 'teste',800.0);
+INSERT INTO despesa (id, data_da_despesa, titulo, valor) VALUES (10, now(), 'teste',800.0);
+INSERT INTO despesa (id, data_da_despesa, titulo, valor) VALUES (11, now(), 'teste',800.0);
+INSERT INTO despesa (id, data_da_despesa, titulo, valor) VALUES (12, now(), 'teste',800.0);
+INSERT INTO despesa (id, data_da_despesa, titulo, valor) VALUES (13, now(), 'teste',800.0);
+INSERT INTO despesa (id, data_da_despesa, titulo, valor) VALUES (14, now(), 'teste',800.0);
+INSERT INTO despesa (id, data_da_despesa, titulo, valor) VALUES (15, now(), 'teste',800.0);
+INSERT INTO despesa (id, data_da_despesa, titulo, valor) VALUES (16, now(), 'teste',800.0);
+INSERT INTO despesa (id, data_da_despesa, titulo, valor) VALUES (17, now(), 'teste',800.0);
